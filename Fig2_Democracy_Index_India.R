e66db723065d5e30bfd0403ad9ede3806777bce2
Sanjay Noronha <sanjaynoronha@Sanjays-MacBook-Air.local>
2022-03-12 09:54:08 +0100

    Using palette 2 + other cosmetic changes to the R file

--- Fig2_Democracy_Index_India.R
+++ Fig2_Democracy_Index_India.R
@@ -5,14 +5,17 @@ library(ggthemes)
 library(dplyr)
 
 # Variables
-partyColor <- c("#F97D09",  "#1e90ff")
-titleColor <- "#EC111A"
+partyColor <- c(Congress = "#02C39A",  BJP = "#D4DC41")
+titleColor <- "#444444"
+footerColor <- "#888888" 
 displayResolution <- 600
-lineColor <- "#758D99"
+lineColor <- "#02C39A"
+pointColor <- "#05648A" 
+
 
 # set working directory and read the file
-setwd("/Users/sanjaynoronha/Desktop/R")
-df <- read.csv("Fig2_Democracy_Index_India.csv")
+setwd("/Users/sanjaynoronha/Desktop/R/Github/R_charts")
+df <- read.csv("Fig2_Democracy_Index_India.csv") 
 
 # print the file to see the contents
 df
@@ -21,17 +24,17 @@ df
 p <- ggplot(df, aes(x=year, y= rank)) +
   geom_rect(data = df, aes(xmin = year, xmax = year_end, fill = party), ymin =-Inf, ymax = Inf, alpha = 0.2) +
   geom_text(aes(label = rank), vjust = - 1.6) +
-  geom_line(color = lineColor) +
-  geom_point(size = 3.5, color = titleColor) +
+  geom_line(colour = lineColor) +
+  geom_point(size = 3, colour = pointColor) +
   scale_y_reverse(limits = c(60, 20)) + #Ref: https://r-graphics.org/recipe-axes-reverse
   scale_x_continuous(  breaks    =c(2006, 2008, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021)
                        , labels = c(2006, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 2021)) +
   scale_fill_manual(values = partyColor) + 
-
-# this is our base theme
-theme_clean() +
   
-# customize the base theme
+  # this is our base theme
+  theme_fivethirtyeight() +
+  
+  # customize the base theme
   labs(
     title = "India, Democracy Index Rank Over the Years"
     , subtitle = "India's rank among 167 countries. Data unavailable: 2007, 9"
@@ -39,13 +42,14 @@ theme_clean() +
   ) +
   
   theme(
-    plot.caption = element_text(hjust=c(1, 0))
-    , plot.title = element_text(colour = titleColor)
+    plot.caption = element_text(hjust=c(1, 0), colour = footerColor)
+    , plot.title = element_text(colour = titleColor, hjust = 0.5)
+    , plot.subtitle = element_text(colour = titleColor, hjust = 0.5)
   ) +
   
   theme(
-    panel.grid.major.x =   element_blank()
-    , panel.grid.major.y =  element_blank()
+    panel.grid.major.y =  element_blank()
+    , panel.grid.major.x =  element_line(size = 0.15)
     , panel.grid.minor.x =  element_blank()
     , panel.grid.minor.y = element_blank()
   ) +
@@ -58,9 +62,15 @@ theme_clean() +
     legend.title = element_blank()
     , legend.position="bottom"
     , legend.background = element_blank()
-  ) 
+  ) +
+  
+  theme(axis.title = element_text(size = 10)) + ylab('Rank') + xlab('Year')
+
+
 
 # save plot in high resolution
-ggsave(plot = p,  width = 5.6, height = 4.8 ,dpi = displayResolution, filename = "Fig2_Democracy_Index_India.png")
+ggsave(plot = p,  width = 6.0, height = 5.2 ,dpi = displayResolution, filename = "Fig2_Democracy_Index_India.png")
+
+
 
 
